dbf35341c051f13dd895ad96aa54207c77a7e2f7
adipurnama <imadeadipurnamayasa@gmail.com>
2022-10-12 16:58:16 +0800

    BelumResponsive

---
+++ application/controllers/Auth.php
@@ -0,0 +1,27 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Auth extends CI_Controller {
+
+	public function __construct() {
+	   parent::__construct();
+
+    }
+
+	public function index()
+	{
+        $data = array(
+            'title'     => 'Trackless Mail',
+            'content'   => 'home/index',
+		);
+		$this->load->view('layout/wrapper', $data);
+	}
+	public function resetpw()
+	{
+        $data = array(
+            'title'     => 'Trackless Mail - Reset Password',
+            'content'   => 'home/resetpw',
+		);
+		$this->load->view('layout/wrapper', $data);
+	}
+}
